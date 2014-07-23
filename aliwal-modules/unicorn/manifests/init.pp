6e7ea13106c959c9196554550862b5fe6e39adf1
Oriol Fitó <oriol@wuaki.tv>
2014-07-23 17:22:47 +0200

    Removed useless notify

--- aliwal-modules/unicorn/manifests/init.pp
+++ aliwal-modules/unicorn/manifests/init.pp
@@ -5,6 +5,5 @@ define unicorn($user, $home, $app_name=$name, $env='development') {
     group   => root,
     mode    => '0755',
     content => template('unicorn/unicorn.erb'),
-    notify  => Service["unicorn_${app_name}_${environment}"],
   }
 }
