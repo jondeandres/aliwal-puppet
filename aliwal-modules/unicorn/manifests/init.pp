86025ad73e2a2c92e3102566cb12b0a5eade578b
Oriol Fitó <oriol@wuaki.tv>
2014-07-23 17:15:17 +0200

    Cosmetic changes at unicorn module

--- aliwal-modules/unicorn/manifests/init.pp
+++ aliwal-modules/unicorn/manifests/init.pp
@@ -1,10 +1,10 @@
-define unicorn($user_name, $home_dir, $project_name, $projects_dir, $env='development') {
-  file { "/etc/init.d/unicorn_${env}_${project_name}":
+define unicorn($user, $home, $app_name=$name, $env='development') {
+  file { "/etc/init.d/unicorn_${env}_${app_name}":
     ensure  => present,
     owner   => root,
     group   => root,
     mode    => '0755',
     content => template('unicorn/unicorn.erb'),
-    notify  => Service["unicorn_${project_name}_${environment}"],
+    notify  => Service["unicorn_${app_name}_${environment}"],
   }
 }
