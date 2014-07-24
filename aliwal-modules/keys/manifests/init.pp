ac39fc5cddce4d6964ce2b4ac26484f92544b9ac
Oriol Fitó <oriol@wuaki.tv>
2014-07-24 12:00:07 +0200

    Moving keys to a define type

--- aliwal-modules/keys/manifests/init.pp
+++ aliwal-modules/keys/manifests/init.pp
@@ -1,36 +1,36 @@
-# Keys for aliwal user
-class keys {
-  if ! defined(User[$::user]) {
-    user { $::user:
+# Set ssh keys for a given user, 'aliwal' by default
+define keys($user='aliwal',$group='aliwal',$home) {
+  if ! defined(User[$user]) {
+    user { $user:
       ensure     => 'present',
-      gid        => $::group,
+      gid        => $group,
       shell      => '/bin/bash',
-      home       => $::home,
+      home       => $home,
       managehome => true
     }
   }
   file {
-    "${::home}/.ssh":
+    "${home}/.ssh":
       ensure  => 'directory',
-      owner   => $::user,
-      group   => $::group,
+      owner   => $user,
+      group   => $group,
       mode    => '0755',
-      require => File[$::home];
+      require => File[$home];
 
-    "${::home}/.ssh/id_dsa":
+    "${home}/.ssh/id_dsa":
       ensure  => present,
       mode    => '0600',
-      owner   => $::user,
-      group   => $::group,
+      owner   => $user,
+      group   => $group,
       source  => 'puppet:///modules/keys/id_dsa',
-      require => File["${::home}/.ssh"];
+      require => File["${home}/.ssh"];
 
-    "${::home}/.ssh/id_dsa.pub":
+    "${home}/.ssh/id_dsa.pub":
       ensure  => present,
       mode    => '0600',
-      owner   => $::user,
-      group   => $::group,
+      owner   => $user,
+      group   => $group,
       source  => 'puppet:///modules/keys/id_dsa.pub',
-      require => File["${::home}/.ssh"];
+      require => File["${home}/.ssh"];
   }
 }
